4a4e0bc2cd6e9ee9d74cc2bad9a94db1f01aa3d5
Kamil <kamilsuhak@gmail.com>
2026-03-04 01:31:42 +0100

    fixed robots import issue

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from "next";
-import { GlobalConfig } from "./config/site-config";
+import { GlobalConfig } from "@/config/site-config";
 
 export default function robots(): MetadataRoute.Robots {
   return {
